50e583687a9a17661af603bda45d72133360c54e
Никифоров Владислав <kit@DAVKLUCHISMRAZ.Dlink>
2021-02-05 14:28:01 +0700

    Fix 2.4

--- problems-2/v-nikiforov/problem4.py
+++ problems-2/v-nikiforov/problem4.py
@@ -1,10 +1,15 @@
+print("Enter sequence to search for.")
+str2find = input()
 f = open('pi.txt', 'r')
 pi = ''.join(f.read()[2:].split('\n'))
-str2find = input()
 index = 0
 co = 0
+res = []
 while (id := pi.find(str2find, index)) != -1:
     index = id + 1
-    print(index - 1)
+    if co < 5:
+        res.append(id)
     co += 1
-print(co)
+
+print(f'Found {co} results.')
+print(f"Positions: {' '.join(map(str, res))}")
